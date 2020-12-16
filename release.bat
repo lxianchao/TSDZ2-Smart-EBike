6181a0aa93f29893973ecd0f3d7cbb05f3072016
lxianchao <920498861@qq.com>
2020-12-16 13:59:43 +0800

    根据自己开发环境修改bat和MakeFlie

--- release.bat
+++ release.bat
@@ -9,10 +9,10 @@ SET release_folder=releases\%new_version%
 IF NOT EXIST %release_folder% (
 	MKDIR %release_folder%
 
-    CD src\controller
+    CD src
     CALL compile.bat || GOTO :error
     CD %home_dir%
-    COPY .\src\controller\main.ihx %release_folder%\TSDZ2-v%new_version%.hex
+    COPY .\src\main.ihx %release_folder%\TSDZ2-v%new_version%.hex
 
     CD src\display\KT-LCD3
     CALL compile.bat || GOTO :error
